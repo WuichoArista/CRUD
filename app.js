78060898c7a8dfd3201b423a7ab353f7f96af000
Luis Arista <luarista.dwfc5@ucamp.utel.edu.mx>
2022-04-05 01:19:08 -0500

    detalles al estar vacio

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ function renderItem(arregloAImprimir){
                                </div>`
            })
     }else{
-        lugar.innerHTML = `
+        let lugarimprimireso = lugar.innerHTML = `
                 <div class="no_hay_producto" id="no_hay_producto">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
                           <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
@@ -112,6 +112,15 @@ function traerDelLocalStorage() {
         let arregloLocalStorage = JSON.parse(localStorage.getItem('productos'))
         if(arregloLocalStorage === null){
             producto = []
+            const lugar = document.getElementById('lugarItem')
+            let lugarimprimireso = lugar.innerHTML = `
+                <div class="no_hay_producto" id="no_hay_producto">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
+                          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+                    </svg>
+                    <h2>No tienes ningun producto agregado</h2>
+                </div>
+        `
         }else {
             producto = arregloLocalStorage
         renderItem(producto)
